5cfcb402bc907beff9472ea27e847bdd27c1321d
Jared Grippe <jared@deadlyicon.com>
2016-10-04 15:43:35 -0700

    WIP

--- browser/components/BoardsDropdown.js
+++ browser/components/BoardsDropdown.js
@@ -3,10 +3,18 @@ import React, { Component } from 'react'
 
 export default class BoardsDropdown extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      open: false
+    }
+  }
+
   render() {
+    const dropdown = this.state.open ? <Dropdown /> : null
     return <div className="BoardsDropdown">
       <button className={this.props.className}>Boards</button>
-      <Dropdown />
+      {dropdown}
     </div>
   }
 
